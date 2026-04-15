51918f3911cc0f5ef27eb4724f2b01449f7edddc
RusnatiAlessio24 <rusnati.alessio@iisgalvanimi.edu.it>
2026-04-15 12:20:01 +0000

    Commit1

---
+++ sprite.js
@@ -0,0 +1,14 @@
+var running = [
+
+"https://i.ibb.co/bgsxt0Jh/Run-005.png",
+"https://i.ibb.co/SXqWk6wP/Run-006.png",
+"https://i.ibb.co/G4t5G9qD/Run-007.png",
+"https://i.ibb.co/0Vrn7sQX/Run-008.png",
+"https://i.ibb.co/Y7v5zpHr/Run-009.png",
+"https://i.ibb.co/20w4n5YZ/Run-000.png",
+"https://i.ibb.co/mCZgz1L6/Run-001.png",
+"https://i.ibb.co/8DVPxKKN/Run-002.png",
+"https://i.ibb.co/k6KSbn5C/Run-003.png",
+"https://i.ibb.co/7t4Q2gzf/Run-004.png",
+
+]
